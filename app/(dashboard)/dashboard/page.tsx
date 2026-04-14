5e1344c27118fec58ff53dd1dad09f2cc9ae8e61
Nicolas Eliazer Jara <nicolas.jara.dev@gmail.com>
2026-04-14 13:58:37 -0300

    haciendo el funcionamiento real de agregar task

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,29 @@
+"use client"
 import { Button } from "@/components/Button";
 import TaskCard from "@/components/TaskCard";
+import { useState } from "react";
 
 export default function Boards() {
+  const [task, setTask] = useState({
+    todo: [],
+    scheduled: [],
+    progress: [],
+    done: [],
+  });
+  const [inputValue , setInputValue] = useState("");
+
+  const addTask = (data) => {
+    const newTask = {
+      id: Date.now(),
+      title: data,
+    };
+
+    setTask((prev) => ({
+      ...prev,
+      todo: [...prev.todo, newTask],
+    }));
+  };
+
   const columns = [
     { id: "todo", title: "New Task" },
     { id: "scheduled", title: "Scheduled" },
@@ -9,23 +31,19 @@ export default function Boards() {
     { id: "done", title: "Completed" },
   ];
 
-  const tasks = {
-    todo: [{ id: 1, title: "Comprar comida" }],
-    scheduled: [{ id: 2, title: "Ir al gym" }],
-    progress: [
-      { id: 3, title: "Estudiar React" },
-      { id: 5, title: "Estudiar React" },
-    ],
-    done: [{ id: 4, title: "Leer libro" }],
-  };
 
   return (
     <div className="w-full">
-
       {/* HEADER */}
-      <div className="flex border-b border-[#e4e4e4] pb-5">
-        <Button className="bg-blue-500">+ Add New</Button>
-      </div>
+      <form onSubmit={(e) => {
+    e.preventDefault();
+    addTask(inputValue);
+  }} className="flex pb-5 ">
+        <input value={inputValue} onChange={(e)=> setInputValue(e.target.value)} placeholder="ingrese task" className="w-89/100 placeholcer: pl-2 "></input>
+        <div className="flex border-b border-[#e4e4e4]">
+          <Button className="bg-blue-500">+ Add New</Button>
+        </div>
+      </form>
 
       {/* COLUMN TITLES */}
       <div className="grid grid-cols-4 mt-5 border-b border-[#e4e4e4] pb-5">
@@ -33,7 +51,7 @@ export default function Boards() {
           <div key={col.id} className="flex items-center gap-2">
             <h1>{col.title}</h1>
             <span className="h-6 w-6 flex items-center justify-center rounded-full bg-[#dfe7e8] text-[#8f959c]">
-              {tasks[col.id].length}
+              {task[col.id].length}
             </span>
           </div>
         ))}
@@ -43,13 +61,12 @@ export default function Boards() {
       <div className="grid grid-cols-4 gap-5 mt-5">
         {columns.map((col) => (
           <div key={col.id} className="space-y-4">
-            {tasks[col.id].map((task) => (
+            {task[col.id].map((task) => (
               <TaskCard key={task.id} title={task.title} />
             ))}
           </div>
         ))}
       </div>
-
     </div>
   );
 }
