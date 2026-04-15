4262a74d080370bf3d66e96ba03186ebd63ea9b6
Nicolas Eliazer Jara <nicolas.jara.dev@gmail.com>
2026-04-15 12:37:08 -0300

    ui terminado

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -4,6 +4,14 @@ import TaskCard from "@/components/TaskCard";
 import { useState } from "react";
 
 export default function Boards() {
+  const statusColors = {
+    todo: "bg-gray-200 text-gray-700",
+    scheduled: "bg-blue-200 text-blue-700",
+    progress: "bg-yellow-200 text-yellow-700",
+    done: "bg-green-200 text-green-700",
+  };
+
+
   const [tasks, setTasks] = useState({
     todo: [],
     scheduled: [],
@@ -11,10 +19,17 @@ export default function Boards() {
     done: [],
   });
 
+  const totalTasks =
+  tasks.todo.length +
+  tasks.scheduled.length +
+  tasks.progress.length +
+  tasks.done.length;
+
   const [inputValue, setInputValue] = useState("");
 
   const addTask = (title) => {
     if (!title.trim()) return;
+
 
     const newTask = {
       id: Date.now(),
@@ -37,15 +52,17 @@ export default function Boards() {
   ];
 
   const deleteTask = (columnId, taskId) => {
+
     setTasks((prev) => ({
       ...prev,
       [columnId]: prev[columnId].filter((t) => t.id !== taskId),
     }));
   };
 
-  // 🔥 mover task
   const selecCol = (fromColumn, taskId, toColumn) => {
     setTasks((prev) => {
+      if (fromColumn === toColumn) return prev; // 🔥 FIX
+
       const task = prev[fromColumn].find((t) => t.id === taskId);
       if (!task) return prev;
 
@@ -59,7 +76,6 @@ export default function Boards() {
 
   return (
     <div className="w-full">
-      {/* HEADER */}
       <form
         onSubmit={(e) => {
           e.preventDefault();
@@ -73,24 +89,29 @@ export default function Boards() {
           placeholder="ingrese task"
           className="w-full pl-2"
         />
-        <Button className="bg-blue-700 hover:bg-blue-600 text-blue-200">
+        <Button
+          type="submit"
+          className="bg-blue-700 hover:bg-blue-600 text-blue-200"
+        >
           + Add New
         </Button>
       </form>
 
-      {/* COLUMN TITLES */}
       <div className="grid grid-cols-4 mt-5 border-b border-[#e4e4e4] pb-5">
         {columns.map((col) => (
           <div key={col.id} className="flex items-center gap-2">
-            <h1>{col.title}</h1>
-            <span className="h-6 w-6 flex items-center justify-center rounded-full bg-[#dfe7e8] text-[#8f959c]">
+            <h1>{col.title}</h1> 
+            <span className={`h-6 w-6 flex items-center justify-center rounded-full ${statusColors[col.id]}`}>
               {tasks[col.id].length}
             </span>
           </div>
         ))}
       </div>
 
-      {/* TASKS */}
+      {totalTasks === 0 ? <div className="text-center pt-10">
+        <h1 className="text-gray-700 bg-gray-200">No hay tareas</h1>
+      </div>
+      :
       <div className="grid grid-cols-4 gap-5 mt-5">
         {columns.map((col) => (
           <div key={col.id} className="space-y-4">
@@ -99,13 +120,14 @@ export default function Boards() {
                 key={task.id}
                 title={task.title}
                 id={task.id}
+                status={col.id}
                 onDelete={(id) => deleteTask(col.id, id)}
                 selecCol={(id, to) => selecCol(col.id, id, to)}
               />
             ))}
           </div>
         ))}
-      </div>
+      </div>}
     </div>
   );
 }
--- components/TaskCard.tsx
+++ components/TaskCard.tsx
@@ -5,11 +5,25 @@ type Props = {
   title: string;
   onDelete: (id: number) => void;
   selecCol: (id: number, to: string) => void;
+  status: string;
 };
 
-export default function TaskCard({ title, onDelete, id, selecCol }: Props) {
+export default function TaskCard({
+  title,
+  onDelete,
+  id,
+  selecCol,
+  status,
+}: Props) {
+  const statusColors = {
+    todo: "bg-gray-200 text-gray-700",
+    scheduled: "bg-blue-200 text-blue-700",
+    progress: "bg-yellow-200 text-yellow-700",
+    done: "bg-green-200 text-green-700",
+  };
+
   return (
-    <div className="bg-gray-200 text-gray-700 rounded-2xl p-4">
+    <div className={`rounded-2xl p-4 ${statusColors[status]}`}>
       <div className="flex items-center justify-between">
         <h1>{title}</h1>
         <Button onClick={() => onDelete(id)}>🗑</Button>
@@ -18,14 +32,14 @@ export default function TaskCard({ title, onDelete, id, selecCol }: Props) {
       <div className="flex justify-center items-center gap-2 mt-2">
         <Button
           onClick={() => selecCol(id, "scheduled")}
-          className="text-yellow-700 text-[10px]"
+          className="text-blue-700 text-[10px]"
         >
           Scheduled
         </Button>
 
         <Button
           onClick={() => selecCol(id, "progress")}
-          className="text-blue-700 text-[10px]"
+          className="text-yellow-700 text-[10px]"
         >
           In progress
         </Button>
